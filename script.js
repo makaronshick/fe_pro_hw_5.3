9df4179c5edbe679e35cdf7d3b0f97094cf72305
makaronshick <makaronshick@ukr.net>
2024-07-17 00:15:37 +0300

    main logic added

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict"
+
+/* 
+Дано ціле число N (ввести через prompt). 
+Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+const START_VALUE = 1;
+const END_VALUE = 100;
+
+const inputValue = prompt('Please enter some number')
+
+if (inputValue === null) {
+  alert("You clicked 'Cancel'");
+}
+else if (!inputValue.trim()) {
+  alert('You entered nothing (or spaces only)');
+}
+else if (isNaN(inputValue)) {
+  alert('You entered not a number');
+}
+else {
+  const absInt = Math.abs(parseInt(inputValue));
+
+  let result = [];
+for (let i = START_VALUE; i <= END_VALUE; i ++) {
+
+  if (Math.pow(i, 2) < absInt) {
+    result.push(i);
+  }
+}
+
+alert(`
+  All of these numbers (from 1 to 100) raised to the power of 2 do not exceed the number you entered (${absInt}):\n ${result.join(', ')}
+  `)
+}
